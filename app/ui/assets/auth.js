d0ed18efd399fb21ce76a155fcde4d3e97dcc88e
Fran Hrženjak <fran.hrzenjak@gmail.com>
2024-04-07 21:52:27 +0200

    auth callbacks with new_user_data intermediary store

--- app/ui/assets/auth.js
+++ app/ui/assets/auth.js
@@ -6,13 +6,38 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
          * This triggers only one time, when the app is first loaded, or possibly also when the user tries
          * to navigate to a new page without being logged in.
          */
-        loadUserData: function(pathname, userData, settings) {
-            if (settings.public_paths.includes(pathname)) {
-                return dash_clientside.no_update;
+        loadUserData: async (pathname, newUserData, userData, settings) => {
+            // another callback set new_user_data, copy it to user_data:
+            if (dash_clientside.callback_context.triggered_id === "new_user_data" && newUserData !== undefined) {
+                return [newUserData, null, dash_clientside.no_update];
             }
+
+            // if no user data, fetch it from backend:
             if (!userData) {
-                return settings.login_path;
+                const responseData = await fetch(settings.backendPath + "api/me/")
+                    .then(response => response.json()).catch(() => null);
+                if (responseData && responseData.email) {
+                    return [responseData, null, dash_clientside.no_update];
+                }
+                return [null, null, "/login/"];
             }
-        }
+
+            // no-op:
+            return [dash_clientside.no_update, dash_clientside.no_update, dash_clientside.no_update];
+        },
+
+        applyUserData: (userData) => {
+            const retData = {
+                userMenuHidden: true,
+                userMenuLabel: "",
+                loginHidden: false,
+            };
+            if (userData) {
+                retData.userMenuLabel = userData.email;
+                retData.userMenuHidden = false;
+                retData.loginHidden = true;
+            }
+            return [retData.userMenuHidden, retData.userMenuLabel, retData.loginHidden];
+        },
     }
 });
--- app/ui/callbacks.py
+++ app/ui/callbacks.py
@@ -6,9 +6,25 @@ def register_auth_callbacks(app: Dash) -> None:
 
     app.clientside_callback(
         ClientsideFunction(clientside_namespace, "loadUserData"),
-        Output("_pages_location", "pathname"),
+        [
+            Output("user_data", "data"),
+            Output("new_user_data", "data", allow_duplicate=True),
+            Output("_pages_location", "pathname", allow_duplicate=True),
+        ],
         Input("url", "pathname"),
+        Input("new_user_data", "data"),
         State("user_data", "data"),
         State("settings", "data"),
+        prevent_initial_call=False,
+    )
+
+    app.clientside_callback(
+        ClientsideFunction(clientside_namespace, "applyUserData"),
+        [
+            Output("user_menu_wrap", "hidden"),
+            Output("user_menu", "label"),
+            Output("login_link_wrap", "hidden"),
+        ],
+        Input("user_data", "data"),
         prevent_initial_call=False,
     )
