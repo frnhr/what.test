0c381699c81e56de548054e0386f97369ca32df6
Fran Hrženjak <fran.hrzenjak@gmail.com>
2024-04-07 21:48:40 +0200

    installed dash extensions; added support for clientside triggers

--- app/ui/app.py
+++ app/ui/app.py
@@ -1,20 +1,37 @@
 import dash_bootstrap_components as dbc
-from dash import Dash
+from dash_extensions.enrich import (
+    DashProxy,
+    NoOutputTransform,
+    TriggerTransform,
+)
 
 from ui.callbacks import register_auth_callbacks
-from ui.clientside_sugar import add_clientside_decorator
+from ui.clientside_sugar import (
+    add_clientside_decorator,
+    enable_dash_extensions_clientside_trigger,
+)
 from ui.layout import get_layout
 
 __all__ = ["app"]
 
 
-app = Dash(
+app = DashProxy(
     __name__,
     use_pages=True,
     external_stylesheets=[dbc.themes.SKETCHY],
+    external_scripts=[
+        "https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js",
+    ],
     prevent_initial_callbacks=True,
+    suppress_callback_exceptions=True,
+    transforms=[
+        # MultiplexerTransform(),
+        TriggerTransform(),
+        NoOutputTransform(),
+    ],
 )
 
+enable_dash_extensions_clientside_trigger()
 add_clientside_decorator(app)
 
 app.layout = get_layout()
--- app/ui/clientside_sugar.py
+++ app/ui/clientside_sugar.py
@@ -1,6 +1,10 @@
-from collections.abc import Callable
+from __future__ import annotations
 
-from dash import Dash
+from collections.abc import Callable
+from itertools import compress
+
+from dash import ClientsideFunction, Dash
+from dash_extensions.enrich import CallbackBlueprint, Trigger, TriggerTransform
 
 __all__ = ["add_clientside_decorator"]
 
@@ -56,3 +60,44 @@ def add_clientside_decorator(app: Dash) -> None:
         return wrapper
 
     app.clientside = clientside_decorator
+
+
+def enable_dash_extensions_clientside_trigger() -> None:
+    """
+    Allow use of Trigger with clientside callbacks.
+
+    https://github.com/emilhe/dash-extensions/pull/318
+    """
+
+    class ClientsideTrigger:
+        def apply_clientside(
+            self,
+            callbacks: list[CallbackBlueprint],
+        ) -> list[CallbackBlueprint]:
+            for callback in callbacks:
+                is_not_trigger = [
+                    not isinstance(item, Trigger) for item in callback.inputs
+                ]
+                # Check if any triggers are there.
+                if all(is_not_trigger):
+                    continue
+                # If so, filter the callback args.
+                args = [f"arg{i}" for i in range(len(callback.inputs))]
+                filtered_args = compress(args, is_not_trigger)
+                if isinstance(callback.f, ClientsideFunction):
+                    callback.f = (
+                        f"window.dash_clientside"
+                        f"['{callback.f.namespace}']"
+                        f"['{callback.f.function_name}']"
+                    )
+                callback.f = f"""function({", ".join(args)}) {{
+const func = {callback.f};
+return func({", ".join(filtered_args)});
+}}"""
+            return callbacks
+
+    if ClientsideTrigger not in TriggerTransform.__bases__:
+        TriggerTransform.__bases__ = (
+            ClientsideTrigger,
+            *TriggerTransform.__bases__,
+        )
